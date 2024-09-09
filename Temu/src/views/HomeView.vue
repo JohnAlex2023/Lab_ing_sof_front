bca5eb51b14c9c666b0e23cd09a4655189144c5f
Sewax <s.gaviria3@utp.edu.co>
2024-09-08 22:52:57 -0500

    Reparación de login + index

--- Temu/src/views/HomeView.vue
+++ Temu/src/views/HomeView.vue
@@ -1,5 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { RouterLink, RouterView } from 'vue-router'
+import MainHeader from '@/components/MainHeader.vue'
+import MainBanner from '@/components/MainBanner.vue'
+import ProductGrid from '@/components/ProductGrid.vue'
+import TheFooter from '@/components/TheFooter.vue'
 </script>
 
 <template>
