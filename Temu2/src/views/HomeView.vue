794c1f2a6200f8d61253274765552779d1cb1de4
Sewax <s.gaviria3@utp.edu.co>
2024-09-08 23:32:38 -0500

    Temu 2 organizado

--- Temu2/src/views/HomeView.vue
+++ Temu2/src/views/HomeView.vue
@@ -1,13 +1,6 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import MainHeader from '@/components/MainHeader.vue'
-import MainBanner from '@/components/MainBanner.vue'
-import ProductGrid from '@/components/ProductGrid.vue'
-import TheFooter from '@/components/TheFooter.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+
 </template>
