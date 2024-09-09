9a1ccf95da54ceaa5bcd2191846075dc0ca8ede3
Sewax <s.gaviria3@utp.edu.co>
2024-09-08 23:25:36 -0500

    Temu 2

---
+++ Temu2/src/App.vue
@@ -0,0 +1,36 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+import MainHeader from './components/MainHeader.vue'
+import MainBanner from './components/MainBanner.vue'
+import ProductGrid from './components/ProductGrid.vue'
+import TheFooter from './components/TheFooter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainHeader,
+    MainBanner,
+    ProductGrid,
+    TheFooter
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <MainHeader />
+    <MainBanner />
+    <ProductGrid />
+    <TheFooter />
+  </div>
+</template>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
