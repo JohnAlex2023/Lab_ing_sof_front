d1e9123360fa603a552b93d20cf7b31303bffb76
JohnAlex2023 <alexander.taimal@utp.du.co>
2024-09-08 23:11:34 -0500

    actualizacion0809

---
+++ Temu/src/views/TheHome.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <MainHeader />
+    <MainBanner />
+    <ProductGrid />
+    <TheFooter />
+  </div>
+</template>
+
+<script>
+import MainHeader from '@/components/MainHeader.vue'
+import MainBanner from '@/components/MainBanner.vue'
+import ProductGrid from '@/components/ProductGrid.vue'
+import TheFooter from '@/components/TheFooter.vue'
+
+export default {
+  name: 'TheHome',
+  components: {
+    MainHeader,
+    MainBanner,
+    ProductGrid,
+    TheFooter
+  }
+}
+</script>
